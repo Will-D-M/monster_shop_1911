605af7249a2e6104635ddfbce955af030d5efffe
DeMarcus Kirby <tkd024@gmail.com>
2020-02-19 10:53:12 -0700

    Add route for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/', to: "welcome#show"
+
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
