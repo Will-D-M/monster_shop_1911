8d4a50295c8fe7f9e56f5ce6912d4b311c8c8f56
William Meighan <williamdmeighan@gmail.com>
2020-02-19 15:15:33 -0700

    Add current_default and current_merchant_employee helper methods
    Co-Authored-By: holmesm8 <holmesm8@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :current_default?
+  helper_method :cart, :current_user, :current_default?, :current_merchant_employee?
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
@@ -9,6 +9,14 @@ class ApplicationController < ActionController::Base
 
   def current_user
     user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_default?
+    current_user && current_user.default?
+  end
+
+  def current_merchant_employee?
+    current_user && current_user.merchant_employee?
   end
 
 end
