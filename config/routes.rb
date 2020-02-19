31d970bb103c218243e39b03f394e442fd489201
William Meighan <williamdmeighan@gmail.com>
2020-02-19 16:31:27 -0700

    Add namespace for merchant and admin
    Co-Authored-By: holmesm8 <holmesm8@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -43,5 +43,12 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
 
+  namespace :merchant do
+    get '/', to: 'dashboard#show'
+  end
+
+  namespace :admin do
+    get '/', to: 'dashboard#index'
+  end
 
 end
