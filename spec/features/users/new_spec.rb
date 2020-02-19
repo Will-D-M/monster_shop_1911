8f048a7fff2ee7bb341ec46bd8fa294e691bdf8b
William Meighan <williamdmeighan@gmail.com>
2020-02-19 16:37:21 -0700

    test for merchant employee not having access to admin and it is already passing
    Co-Authored-By: holmesm8 <holmesm8@gmail.com>

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -172,4 +172,21 @@ RSpec.describe "create user page", type: :feature do
       expect(page).to have_content("The page you were looking for doesn't exist")
     end
   end
+
+  describe 'as a merchant user' do
+    it 'cannot visit merchant or admin pages' do
+      user = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "sfgdfg", role: 1)
+
+      visit '/'
+
+      click_link 'Log In'
+
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_on 'Login'
+
+      visit '/admin'
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
 end
