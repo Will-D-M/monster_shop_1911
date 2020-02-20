6e3d8a6ed71745fb6908cfd9bf2f140330f4f810
DeMarcus Kirby <tkd024@gmail.com>
2020-02-19 19:40:19 -0700

    update method name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,20 @@
 class SessionsController < ApplicationController
-<<<<<<< HEAD
   def new
-    if current_user && current_user.admin?
+    if current_admin?
       flash[:notice] = 'You are already logged in!'
       redirect_to '/admin'
-    elsif current_user && current_user.merchant?
+    elsif current_merchant?
       flash[:notice] = 'You are already logged in!'
       redirect_to '/merchant'
     elsif current_user
       flash[:notice] = 'You are already logged in!'
       redirect_to "/profile"
     end
-=======
-
-  def new
->>>>>>> master
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
-<<<<<<< HEAD
       session[:user] = user.id
       flash[:notice] = "Login Successful"
       if user.admin?
@@ -33,15 +27,14 @@ class SessionsController < ApplicationController
     else
       flash[:notice] = "Invalid Email and/or Password"
       redirect_to "/login"
-=======
-      session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.name}!"
-      redirect_to '/'
-    else
-      flash[:error] = "Sorry, your credentials are bad."
-      render :new
->>>>>>> master
     end
+  end
+
+  def destroy
+    session.delete(:user)
+    session.delete(:cart)
+    flash[:notice] = 'You have logged out'
+    redirect_to "/"
   end
 
 end
