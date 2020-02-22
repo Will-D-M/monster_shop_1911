b7cdde7dfdb322d62b5795e54bd5385063b95248
William Meighan <williamdmeighan@gmail.com>
2020-02-22 14:49:50 -0700

    Create test for My Orders link
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "As a registered user on the profile page" do
   before :each do
     @user = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "test", role: 0)
+    @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
 
     visit '/login'
 
@@ -21,6 +23,7 @@ describe "As a registered user on the profile page" do
     expect(page).to have_content("Zip: #{@user.zip}")
     expect(page).to have_content("Email: #{@user.email}")
     expect(page).to have_link("Edit Info")
+    expect(page).to_not have_link("My Orders")
   end
 
   it 'I can click link to edit profile' do
@@ -63,6 +66,33 @@ describe "As a registered user on the profile page" do
 
     expect(current_path).to eq("/profile")
     expect(page).to have_content("Your password has been updated")
+  end
+
+  it 'I see a link to my orders if I have made any' do
+    visit "/items/#{@pencil.id}"
+    click_on "Add To Cart"
+    visit "/cart"
+
+    click_on "Checkout"
+
+    name = "Bert"
+    address = "123 Sesame St."
+    city = "NYC"
+    state = "New York"
+    zip = 10001
+
+    fill_in :name, with: name
+    fill_in :address, with: address
+    fill_in :city, with: city
+    fill_in :state, with: state
+    fill_in :zip, with: zip
+
+    click_button "Create Order"
+
+    visit '/profile'
+
+    click_link("My Orders")
+    expect(current_path).to eq("/profile/orders")
   end
 
 end
