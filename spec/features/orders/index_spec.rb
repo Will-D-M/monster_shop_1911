7293f32feefea41a37f324f522b80f99d036cf75
William Meighan <williamdmeighan@gmail.com>
2020-02-22 16:20:12 -0700

    Create test for updated index view
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- spec/features/orders/index_spec.rb
+++ spec/features/orders/index_spec.rb
@@ -12,34 +12,39 @@ RSpec.describe "As a user on the order index page" do
 
     @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
     @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-
-    visit "/items/#{@pencil.id}"
-    click_on "Add To Cart"
-    visit "/cart"
-
-    click_on "Checkout"
-
-    name = "Bert"
-    address = "123 Sesame St."
-    city = "NYC"
-    state = "New York"
-    zip = 10001
-
-    fill_in :name, with: name
-    fill_in :address, with: address
-    fill_in :city, with: city
-    fill_in :state, with: state
-    fill_in :zip, with: zip
-
-    click_button "Create Order"
+    @order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
+    @order_1.item_orders.create!(item: @pencil, price: @pencil.price, quantity: 2)
   end
 
-  it "the show page contains the order details" do
-    order = @pencil.orders.create(name: 'Will', address: '123', city: 'Denver', state: 'CO', zip: '80204')
-    expect(current_path).to eq("/profile/orders")
-    expect(page).to have_content("Order has been created.")
-    expect(page).to have_content("Order ID: #{order.id}")
+  it "the page contains the order details" do
+
+    visit("/profile/orders")
+
+    expect(page).to have_content("Order ID: #{@order_1.id}")
+    expect(page).to have_content("Order date: #{@order_1.created_at}")
+    expect(page).to have_content("Last update: #{@order_1.updated_at}")
+    expect(page).to have_content("Current status: #{@order_1.status}")
+    expect(page).to have_content("Total items: #{@order_1.total_items}")
+
     expect(page).to have_content("Cart: 0")
+  end
+
+  it 'displays a message when an order is created' do
+    visit "/items/#{@pencil.id}"
+    click_on "Add To Cart"
+
+    visit "cart"
+    click_on "Checkout"
+
+    fill_in :name, with: "Will"
+    fill_in :address, with: "Will"
+    fill_in :city, with: "Will"
+    fill_in :state, with: "Will"
+    fill_in :zip, with: "Will"
+
+    click_on "Create Order"
+
+    expect(page).to have_content("Order has been created.")
   end
 
 end
