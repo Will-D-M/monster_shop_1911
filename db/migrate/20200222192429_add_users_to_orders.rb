050e9b24375b4358f0ae6039dd7953ce5b6e78c2
William Meighan <williamdmeighan@gmail.com>
2020-02-22 12:25:34 -0700

    Create one to many relationship with order and users
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

---
+++ db/migrate/20200222192429_add_users_to_orders.rb
@@ -0,0 +1,5 @@
+class AddUsersToOrders < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :orders, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200219234144) do
+ActiveRecord::Schema.define(version: 20200222192429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20200219234144) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -81,5 +83,6 @@ ActiveRecord::Schema.define(version: 20200219234144) do
   add_foreign_key "item_orders", "items"
   add_foreign_key "item_orders", "orders"
   add_foreign_key "items", "merchants"
+  add_foreign_key "orders", "users"
   add_foreign_key "reviews", "items"
 end
