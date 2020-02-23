40dee74b80157ffec4aebfa30ecc81c9c04fa047
William Meighan <williamdmeighan@gmail.com>
2020-02-23 10:36:51 -0700

    Create model test for increase and decrease inventory
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -19,6 +19,8 @@ describe Item, type: :model do
 
   describe "instance methods" do
     before(:each) do
+      @user = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "qboy@hotmail.com", password: "sfgdfg", role: 0)
+
       @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
       @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
@@ -32,6 +34,8 @@ describe Item, type: :model do
       @z = @bike_shop.items.create(name: "z", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 10)
       @a = @bike_shop.items.create(name: "a", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 10)
       @b = @bike_shop.items.create(name: "b", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 10)
+
+      @order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
 
       @review_1 = @chain.reviews.create(title: "Great place!", content: "They have great bike stuff and I'd recommend them to anyone.", rating: 5)
       @review_2 = @chain.reviews.create(title: "Cool shop!", content: "They have cool bike stuff and I'd recommend them to anyone.", rating: 4)
@@ -54,43 +58,40 @@ describe Item, type: :model do
 
     it 'no orders' do
       expect(@chain.no_orders?).to eq(true)
-      user = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "sfgdfg", role: 0)
-      order = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order.item_orders.create(item: @chain, price: @chain.price, quantity: 2)
       expect(@chain.no_orders?).to eq(false)
     end
 
     it "returns item with most orders" do
-      user = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "sfgdfg", role: 0)
-
-      order_1 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_1 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_1.item_orders.create(item: @chain, price: @chain.price, quantity: 5)
 
-      order_2 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_2 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_2.item_orders.create(item: @frame, price: @frame.price, quantity: 4)
 
-      order_3 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_3 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_3.item_orders.create(item: @wheel, price: @wheel.price, quantity: 3)
 
-      order_4 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_4 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_4.item_orders.create(item: @seat, price: @seat.price, quantity: 2)
 
-      order_5 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_5 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_5.item_orders.create(item: @handle, price: @handle.price, quantity: 1)
 
-      order_10 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_10 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_10.item_orders.create(item: @x, price: @x.price, quantity: 10)
 
-      order_9 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_9 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_9.item_orders.create(item: @y, price: @y.price, quantity: 9)
 
-      order_8 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_8 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_8.item_orders.create(item: @z, price: @z.price, quantity: 8)
 
-      order_7 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_7 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_7.item_orders.create(item: @a, price: @a.price, quantity: 7)
 
-      order_6 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_6 = Order.create(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: @user.id)
       order_6.item_orders.create(item: @b, price: @b.price, quantity: 6)
 
       expected = [@x, @y, @z, @a, @b]
@@ -98,6 +99,18 @@ describe Item, type: :model do
 
       expected_2 = [@handle, @seat, @wheel, @frame, @chain]
       expect(Item.least_ordered_items).to eq(expected_2)
+    end
+
+    it "decrease_inventory" do
+      @item_order_1 = @order_1.item_orders.create!(item: @chain, price: @chain.price, quantity: 2)
+      @chain.decrease_inventory(@item_order_1.quantity)
+      expect(@chain.inventory).to eq(3)
+    end
+
+    it "increase_inventory" do
+      @item_order_1 = @order_1.item_orders.create!(item: @chain, price: @chain.price, quantity: 2)
+      @chain.increase_inventory(@item_order_1.quantity)
+      expect(@chain.inventory).to eq(7)
     end
   end
 end
