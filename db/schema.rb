2453725b3e24f6458801e7fe5f37892d7abb92c0
William Meighan <williamdmeighan@gmail.com>
2020-02-23 10:37:18 -0700

    Update database with enum to itemorders
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200222204636) do
+ActiveRecord::Schema.define(version: 20200223162124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20200222204636) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["item_id"], name: "index_item_orders_on_item_id"
     t.index ["order_id"], name: "index_item_orders_on_order_id"
   end
