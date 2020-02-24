ddd74a04bf72f6fd4d24cf2159af86a90d180844
William Meighan <williamdmeighan@gmail.com>
2020-02-24 15:49:17 -0700

    add item status to if condition
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -8,8 +8,10 @@ class Admin::MerchantsController < Admin::BaseController
     merchant = Merchant.find(params[:id])
     merchant.change_status
     if merchant.status == "disabled"
+      merchant.items.each { |item| item.active_false }
       flash[:success] = "#{merchant.name} is now disabled."
     else
+      merchant.items.each { |item| item.active_true }
       flash[:success] = "#{merchant.name} is now enabled."
     end
     redirect_to "/admin/merchants"
