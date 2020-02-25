2bd07ca5b1aca99316304334615242be59d62c50
DeMarcus Kirby <tkd024@gmail.com>
2020-02-25 11:37:08 -0700

    Add swap_active? method to handle...the swap of active

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -2,4 +2,22 @@ class Merchant::ItemsController < Merchant::BaseController
   def index
     @items = current_user.merchant.items
   end
+
+  def update
+    item = Item.find(params[:id])
+    swap_active? item if params[:status]
+    redirect_to "/merchant/items"
+  end
+
+  private
+
+    def swap_active? item
+      item.toggle(:active?)
+      if params[:status] == 'deactivate'
+        flash[:notice] = "#{item.name} is no longer for sale."
+      elsif params[:status] == 'activate'
+        flash[:notice] = "#{item.name} is now available for sale."
+      end
+      item.save
+    end
 end
