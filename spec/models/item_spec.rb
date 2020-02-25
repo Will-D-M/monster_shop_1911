ca69ff01984d6bf2274821c9281564e4f4ae4269
William Meighan <williamdmeighan@gmail.com>
2020-02-25 12:37:45 -0700

    Remove required image field
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,6 @@ describe Item, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
     it { should validate_presence_of :price }
-    it { should validate_presence_of :image }
     it { should validate_presence_of :inventory }
     it { should validate_inclusion_of(:active?).in_array([true,false]) }
   end
