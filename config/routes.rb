08b652487515b7bb18f3c9e99b2df024e5072f21
William Meighan <williamdmeighan@gmail.com>
2020-02-25 16:10:05 -0700

    Add routes
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,8 @@ Rails.application.routes.draw do
     delete '/items/:item_id', to: "items#destroy"
     get '/items/new', to: "items#new"
     post '/items', to: "items#create"
+    get '/items/:item_id/edit', to: "items#edit"
+    patch '/items/:item_id', to: "items#update"
   end
 
   namespace :admin do
