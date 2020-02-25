35527ccd034bca0a90c655fe919c6ac3df3078f7
William Meighan <williamdmeighan@gmail.com>
2020-02-25 12:25:51 -0700

    Add controller actions for create/destroy
    Co-Authored-By: Matt Holmes <holmesm8@gmail.com>

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,5 +1,35 @@
 class Merchant::ItemsController < Merchant::BaseController
+
   def index
     @items = current_user.merchant.items
+  end
+
+  def new
+    @merchant = current_user.merchant
+  end
+
+  def create
+    @merchant = current_user.merchant
+    item = @merchant.items.create(item_params)
+    if item.save
+      flash[:success] = "#{item.name} has saved."
+      redirect_to "/merchant/items"
+    else
+      flash[:error] = item.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:item_id])
+    item.destroy
+    flash[:success] = "#{item.name} has been deleted."
+    redirect_to '/merchant/items'
+  end
+
+  private
+
+  def item_params
+    params.permit(:name,:description,:price,:inventory,:image)
   end
 end
