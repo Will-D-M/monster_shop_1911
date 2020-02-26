9b0cb1ff159debd17fae2ad961603aebce11f876
William Meighan <williamdmeighan@gmail.com>
2020-02-26 13:05:22 -0700

    Add model test for pending orders

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -60,5 +60,17 @@ describe Merchant, type: :model do
       expect(@meg.status).to eq("active")
     end
 
+    it 'pending orders' do
+      user = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "sfgdfg", role: 0)
+      chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 40, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 22)
+      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: user.id)
+      order_2 = Order.create!(name: 'Brian', address: '123 Brian Ave', city: 'Denver', state: 'CO', zip: 17033, user_id: user.id)
+      order_3 = Order.create!(name: 'Dao', address: '123 Mike Ave', city: 'Denver', state: 'CO', zip: 17033, user_id: user.id)
+      order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      order_2.item_orders.create!(item: chain, price: chain.price, quantity: 2)
+      order_3.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      expect(@meg.pending_orders.length).to eq(3)
+    end
+
   end
 end
