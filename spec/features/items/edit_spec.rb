7aa7229c866c3043ad23b700c46d415c706664e3
DeMarcus Kirby <tkd024@gmail.com>
2020-02-27 15:05:23 -0700

    Add admin stub for login
    Co-Authored-By: Will Meighan <Will-Meighan@users.noreply.github.com>
    Co-Authored-By: Matt <holmesm8@gmail.com>

--- spec/features/items/edit_spec.rb
+++ spec/features/items/edit_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "As a Visitor" do
       it 'I can see the prepopulated fields of that item' do
         @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
         @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        user_1 = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "test", role: 2)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
         visit "/items/#{@tire.id}"
 
@@ -25,6 +27,8 @@ RSpec.describe "As a Visitor" do
       it 'I can change and update item with the form' do
         @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
         @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        user_1 = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "test", role: 2)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
         visit "/items/#{@tire.id}"
 
@@ -52,6 +56,8 @@ RSpec.describe "As a Visitor" do
       it 'I get a flash message if entire form is not filled out' do
         @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
         @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        user_1 = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "test", role: 2)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
         visit "/items/#{@tire.id}"
 
