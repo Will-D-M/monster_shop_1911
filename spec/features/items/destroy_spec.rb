3b641dab5a327ab4c0c1629e5ae663f327fddbe3
DeMarcus Kirby <tkd024@gmail.com>
2020-02-27 15:05:18 -0700

    Add admin stub for login
    Co-Authored-By: Will Meighan <Will-Meighan@users.noreply.github.com>
    Co-Authored-By: Matt <holmesm8@gmail.com>

--- spec/features/items/destroy_spec.rb
+++ spec/features/items/destroy_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'item delete', type: :feature do
   describe 'when I visit an item show page' do
     it 'I can delete an item' do
+      user_1 = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "test", role: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
 
@@ -20,6 +22,8 @@ RSpec.describe 'item delete', type: :feature do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
       review_1 = chain.reviews.create(title: "Great place!", content: "They have great bike stuff and I'd recommend them to anyone.", rating: 5)
+      user_1 = User.create!(name: "Tommy", address: "123", city: "Bruh", state: "CO", zip: "99999", email: "zboy@hotmail.com", password: "test", role: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
       visit "/items/#{chain.id}"
 
