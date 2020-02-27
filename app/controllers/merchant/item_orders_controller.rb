3197d0ab4c398d605b03f40a788e32bb1ef8ea99
DeMarcus Kirby <tkd024@gmail.com>
2020-02-27 15:06:28 -0700

    Update to call order method for packaged status update and save
    Co-Authored-By: Will Meighan <Will-Meighan@users.noreply.github.com>
    Co-Authored-By: Matt <holmesm8@gmail.com>

--- app/controllers/merchant/item_orders_controller.rb
+++ app/controllers/merchant/item_orders_controller.rb
@@ -3,6 +3,8 @@ class Merchant::ItemOrdersController < Merchant::BaseController
     item_order = ItemOrder.find(params[:id])
     item_order.update(status: 1)
     item_order.save
+    item_order.order.packaged
+    item_order.order.save
     flash[:notice] = "#{item_order.item.name} has been fulfilled."
     redirect_to "/merchant/orders/#{item_order.order_id}"
   end
