abff8f3bb2dc94408c023b8fc9362f24a89b4f6f
Matt Holmes <holmesm8@gmail.com>
2020-03-01 17:19:05 -0700

    Finish CRUD functionality for bulk discounts

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,6 +6,10 @@ class Merchant::DiscountsController < Merchant::BaseController
   def new
   end
 
+  def show
+    @discount = Discount.find(params[:id])
+  end
+
   def create
     @merchant = current_user.merchant
     @discount = @merchant.discounts.new(discount_params)
