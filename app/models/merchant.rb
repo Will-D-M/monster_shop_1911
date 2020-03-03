94dacbe02740f7c4e1ba75aafd29981150a7958c
William Meighan <williamdmeighan@gmail.com>
2020-03-03 16:29:02 -0700

    Add discount relationship to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,7 @@ class Merchant <ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :users
   has_many :item_orders, through: :items
+  has_many :discounts
 
   validates_presence_of :name,
                         :address,
